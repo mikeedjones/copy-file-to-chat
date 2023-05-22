41df297798bf8ddb091b9d07a14c6b21c0727537
JONEMI19 <michael.jones4@kingfisher.com>
2023-05-22 08:47:21 +0100

    inital commit

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('copy-file-for-chat.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Copy file for chat!');
+		vscode.window.showInformationMessage('Hello World from copy-file-for-chat!');
 	});
 
 	context.subscriptions.push(disposable);
