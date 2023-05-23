9b360ab1c4c4a3b6a67f5d2c1d318b9dcf392e86
JONEMI19 <michael.jones4@kingfisher.com>
2023-05-23 17:11:51 +0100

    updated to use .gitignore to exculde files

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
 # Copy File for Chat
 
-This Visual Studio Code extension allows you to copy the content of a file to your clipboard, formatted for pasting into a chat window.
+This Visual Studio Code extension allows you to copy the content of a file to your clipboard, formatted for pasting into the chat window.
 
 ## Features
 
 - Copy the content of a file to your clipboard, formatted for pasting into a chat window
-- Supports TypeScript and JavaScript files
 - Integrates with the context menu in the Explorer view
 
 ## Usage
 
-1. Open the file you want to copy in the editor
-2. Right-click on the file in the Explorer view
-3. Select "Copy file to chat" from the context menu
-4. Paste the formatted content into your chat window
+Two commands are provided:
+
+### Copy File Contents to chat
+
+1. Right-click on the file in the Explorer view
+2. Select "Copy File Contents to chat" from the context menu
+3. The formatted content will be pasted into your chat window
+
+You can also use the command palette to run the "Copy File Contents to chat" command.
+
+### Copy Directory Contents to chat
+
+1. Right-click on the directory in the Explorer view
+2. Select "Copy Directory Contents to chat" from the context menu
+
+The formatted content of all files in the directory will be pasted into your chat window. It will follow 
+the .gitignore rules for the directory, so you can exclude files from being copied by adding them to the .gitignore file.
 
 ## Requirements
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,144 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+import { minimatch } from 'minimatch';
+
+async function copyFile(uri: vscode.Uri) {
+  let content = await vscode.workspace.fs.readFile(uri);
+
+  let filePath = vscode.workspace.asRelativePath(uri);
+  let projectName = vscode.workspace.name || "Unknown";
+  let fileExtension = path.extname(uri.fsPath).slice(1);
+
+  return `
+===============================
+Project Name: '${projectName}'
+File Path: '${filePath}'
+File Content:
+\`\`\`${fileExtension}
+${content}
+\`\`\`
+===============================
+`;
+}
+
+async function shouldIgnoreFile(fileUri: vscode.Uri) {
+    let relativePath = vscode.workspace.asRelativePath(fileUri);
+    let directories = fileUri.fsPath.split(path.sep);
+    
+    let repoRoot;
+    for (let i = 0; i < directories.length; i++) {
+      let dirPath = directories.slice(0, i + 1).join(path.sep);
+      let gitUri = vscode.Uri.joinPath(vscode.Uri.file(dirPath), ".git");
+      
+      // Check if we found the repo root
+      try {
+        // Check if we found the repo root
+        let stat = await vscode.workspace.fs.stat(gitUri);
+        if (stat.type === vscode.FileType.Directory) {
+          repoRoot = dirPath;
+          break;  
+        }
+      } catch (err) {
+        // Ignore - .git folder does not exist, continue traversing up
+      }
+    }
+    
+    for (let i = 0; i < directories.length && directories[i] !== repoRoot; i++) {
+      let dirPath = directories.slice(0, i + 1).join(path.sep);
+      let gitignoreUri = vscode.Uri.joinPath(vscode.Uri.file(dirPath), ".gitignore");
+      
+      try {
+        let gitignoreContent = await vscode.workspace.fs.readFile(gitignoreUri);
+        let gitignore = gitignoreContent.toString().split("\n");
+        for (let pattern of gitignore) {
+          if (pattern.trim() === "" || pattern.startsWith("#")) {
+            continue;
+          }
+          if (minimatch(relativePath, pattern)) {
+            return true;
+          }
+        }
+      } catch (err) {}
+    }
+    
+    return false;
+  }
+
+async function readDirectoryRecursive(uri: vscode.Uri): Promise<vscode.Uri[]> {
+    let files = await vscode.workspace.fs.readDirectory(uri);
+    let fileUris: vscode.Uri[] = [];
+  
+    // Read the .gitignore file if it exists
+    let gitignoreUri = vscode.Uri.joinPath(uri, ".gitignore");
+    let gitignore: string[] = [];
+    try {
+      let gitignoreContent = await vscode.workspace.fs.readFile(gitignoreUri);
+      gitignore = gitignoreContent.toString().split("\n");
+    } catch (err) {}
+  
+    for (let [fileName, fileType] of files) {
+      let fileUri = vscode.Uri.joinPath(uri, fileName);
+      let relativePath = vscode.workspace.asRelativePath(fileUri);
+  
+      // Check if the file should be ignored
+      let ignore = await shouldIgnoreFile(fileUri);
+      if (ignore) {
+        continue;
+      }
+  
+      if (fileType === vscode.FileType.Directory) {
+        let subDirectoryFiles = await readDirectoryRecursive(fileUri);
+        fileUris.push(...subDirectoryFiles);
+      } else if (fileType === vscode.FileType.File) {
+        fileUris.push(fileUri);
+      }
+    }
+  
+    return fileUris;
+  }
+
+async function copyDirectory(uri: vscode.Uri) {
+  let directoryPath = vscode.workspace.asRelativePath(uri);
+  let projectName = vscode.workspace.name || "Unknown";
+
+  let toCopy = `
+===============================
+Project Name: '${projectName}'
+Directory Path: '${directoryPath}'
+===============================
+`;
+
+  let fileUris = await readDirectoryRecursive(uri);
+  for (let fileUri of fileUris) {
+    toCopy += await copyFile(fileUri);
+  }
+
+  return toCopy;
+}
 
 export function activate(context: vscode.ExtensionContext) {
-    // Register a command to send the content of the selected file to this chat window
-    let disposable = vscode.commands.registerCommand('copy-file-to-chat.sendSelectedFileContent', (uri: vscode.Uri) => {
-        // Get the selected file path
-        let filePath = uri.fsPath;
+  let disposableFile = vscode.commands.registerCommand(
+    "extension.sendSelectedFileContent",
+    async (uri: vscode.Uri) => {
+        let toCopy = await copyDirectory(uri);
+        await vscode.env.clipboard.writeText(toCopy);
+        await vscode.commands.executeCommand("workbench.panel.interactiveSession.view.copilot.focus");
+        vscode.commands.executeCommand('editor.action.clipboardPasteAction');
+    }
+  );
 
-        // Read the content of the selected file
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                vscode.window.showErrorMessage(`Failed to read file: ${err.message}`);
-                return;
-            }
+  let disposableDirectory = vscode.commands.registerCommand(
+    "extension.sendSelectedDirectoryContent",
+    async (uri: vscode.Uri) => {
+        let toCopy = await copyDirectory(uri);
+        await vscode.env.clipboard.writeText(toCopy);
+        await vscode.commands.executeCommand("workbench.panel.interactiveSession.view.copilot.focus");
+        vscode.commands.executeCommand('editor.action.clipboardPasteAction');
+    }
+  );
 
-            // Get the workspace folder that contains the selected file
-            let workspaceFolder = vscode.workspace.getWorkspaceFolder(uri);
-            if (!workspaceFolder) {
-                vscode.window.showErrorMessage('Failed to get workspace folder');
-                return;
-            }
-
-            // Get the relative path of the selected file within the workspace folder
-            let relativePath = vscode.workspace.asRelativePath(filePath);
-
-            // Format the message to include the file path and content
-            let message = `Content of \`${relativePath}\`:\n\`\`\`\n${data}\n\`\`\`\n\n`;
-
-            // Send the message to this chat window
-            vscode.env.clipboard.writeText(message);
-            vscode.commands.executeCommand("workbench.panel.interactiveSession.view.copilot.focus");
-            vscode.commands.executeCommand('editor.action.clipboardPasteAction');
-        });
-    });
-
-    // Add the command to the context menu
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposableFile, disposableDirectory);
 }
 
 export function deactivate() {}
